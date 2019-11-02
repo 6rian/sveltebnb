3d208521d49b4b02fad9b8c273b571c2607803c8
Brian Griffin <wbgriffin@gmail.com>
2019-11-02 13:39:13 -0400

    Added login and signup modals

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,5 +1,12 @@
 <script>
-	import Nav from '../components/Nav.svelte';
+	import Nav from '../components/Nav.svelte'
+	import Modal from '../components/Modal.svelte'
+	import LoginModal from '../components/LoginModal.svelte'
+	import RegistrationModal from '../components/RegistrationModal.svelte'
+
+	let showModal = false
+	let showLoginModal = false
+	let showRegistrationModal = false
 
 	export let segment;
 </script>
@@ -20,3 +27,25 @@
 <main>
 	<slot></slot>
 </main>
+
+{#if showModal}
+	<Modal on:close={() => showModal = false}>
+		{#if showLoginModal}
+			<LoginModal
+				on:showSignup={() => {
+					showLoginModal = false
+					showRegistrationModal = true
+				}}
+			></LoginModal>
+		{/if}
+		{#if showRegistrationModal}
+			<RegistrationModal
+				on:showLogin={() => {
+					showRegistrationModal = false
+					showLoginModal = true
+				}}
+			>
+			</RegistrationModal>
+		{/if}
+	</Modal>
+{/if}
